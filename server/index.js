1b026668b0eeb584565651d63b5c4093ce0f91b8
lancerutkin <lance.rutkin@gmail.com>
2019-08-01 19:21:16 -0400

    Build router

--- server/index.js
+++ server/index.js
@@ -1,1 +1,30 @@
-console.log('Reviews service under construction');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const router = require('./routes');
+
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+
+const port = 3000;
+
+const defaultCorsHeaders = {
+  'Access-Control-Allow-Origin': '*',
+  'access-control-allow-methods': 'GET, POST, PUT, OPTIONS',
+  'access-control-allow-headers': 'content-type, accept',
+  'access-control-max-age': 10
+};
+
+app.all('/reviews', (request, response, next) => {
+  response.set(defaultCorsHeaders);
+  next();
+});
+
+app.use('/reviews', router);
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+})
+
+
---
+++ server/routes.js
@@ -0,0 +1,12 @@
+const { getReviews, getMeta, postReview, putHelpfulness, putReported } = require('./controllers');
+const router = require('express').Router();
+
+router.get('/:product_id/list', getReviews);
+
+router.get('/:product_id/meta', getMeta);
+
+router.post('/:product_id', postReview);
+
+router.put('/helpful/:review_id', putHelpfulness);
+
+router.put('/report/:review_id', putReported);
