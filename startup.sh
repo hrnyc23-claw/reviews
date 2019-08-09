e0139395bc20f98a9ac30eb1126e41e4c8410abf
lancerutkin <lance.rutkin@gmail.com>
2019-08-09 15:05:27 -0400

    Edit startup script

--- startup.sh
+++ startup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cd ..
 sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 3000
 sudo yum install gcc -y
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
